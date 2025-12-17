7a3bd4d1f69c294b85fbf3667da2b3d5a8bd1a45
Ethan Hann <ethanhann@gmail.com>
2025-12-16 22:41:41 -0500

    Remove commented-out `prelude` module from `snakeway-core` to clean up unused code.

--- snakeway-core/src/lib.rs
+++ snakeway-core/src/lib.rs
@@ -5,19 +5,3 @@ pub mod http_event;
 pub mod logging;
 mod proxy;
 pub mod server;
-
-// #[allow(unused_imports)]
-// pub mod prelude {
-//     use super::config::SnakewayConfig;
-//     use super::logging::{LogMode, default_log_mode, init_logging};
-//     use super::device::core::Device;
-//     use super::device::wasm::wasm_device::WasmDevice;
-//     use super::ctx::RequestCtx;
-// }
-
-
-//     use snakeway_core::config::SnakewayConfig;
-//     use snakeway_core::logging::{LogMode, default_log_mode, init_logging};
-//     use snakeway_core::device::core::Device;
-//     use snakeway_core::device::wasm::wasm_device::WasmDevice;
-//     use snakeway_core::ctx::RequestCtx;
