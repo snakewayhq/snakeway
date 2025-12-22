032fc548b0bb7c72dd1a0fc2090b42e47148adb4
Ethan Hann <ethanhann@gmail.com>
2025-12-22 18:45:14 -0500

    Rework basic test

--- integration-tests/tests/basic_proxy.rs
+++ integration-tests/tests/basic_proxy.rs
@@ -1,23 +1,30 @@
+use std::sync::Once;
+use std::time::Duration;
+
 mod common;
 
-use snakeway_core::server::build_pingora_server;
-use std::{thread, time::Duration};
+static SERVER: Once = Once::new();
+static CONFIG: &str = "basic.toml";
 
 #[test]
 fn basic_proxy_works() {
     // Arrange
     common::start_upstream();
-    let cfg = common::load_config("basic.toml");
-    let server = build_pingora_server(cfg).unwrap();
-    thread::spawn(move || {
-        server.run_forever();
-    });
-    thread::sleep(Duration::from_millis(100)); // Give it a moment to bind
+    common::start_server(&SERVER, CONFIG);
+    common::wait_for_server("127.0.0.1:4040", Duration::from_secs(2));
+    let client = reqwest::blocking::Client::builder()
+        .timeout(Duration::from_secs(2))
+        .build()
+        .unwrap();
 
     // Act
-    let res = reqwest::blocking::get("http://127.0.0.1:4040/").expect("request failed");
+    let res = client
+        .get("http://127.0.0.1:4040")
+        .send()
+        .expect("request failed");
 
     // Assert
+    assert_eq!(res.status(), 200);
     let body = res.text().unwrap();
     assert_eq!(body, "hello world");
 }
