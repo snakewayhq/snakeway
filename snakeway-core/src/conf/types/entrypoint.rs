43ac8e688b8369e768db3c7b06f117c62f9e16e8
Ethan Hann <ethanhann@gmail.com>
2025-12-25 11:47:25 -0500

    Add comments

--- snakeway-core/src/conf/types/entrypoint.rs
+++ snakeway-core/src/conf/types/entrypoint.rs
@@ -1,6 +1,7 @@
 use crate::conf::types::ServerConfig;
 use serde::Deserialize;
 
+/// Represents the top-level configuration file.
 #[derive(Debug, Deserialize)]
 pub struct EntrypointConfig {
     pub server: ServerConfig,
@@ -8,6 +9,8 @@ pub struct EntrypointConfig {
     pub include: IncludeConfig,
 }
 
+/// Represents the include section of the top-level config file.
+/// The members are directory paths where sub-configuration files are located.
 #[derive(Debug, Deserialize)]
 pub struct IncludeConfig {
     pub routes: String,
