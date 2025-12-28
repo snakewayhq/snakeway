027baa3389e4d0185c4b231bed26c01e11bf37d0
Ethan Hann <ethanhann@gmail.com>
2025-12-28 17:43:43 -0500

    Rename `hash` to `sticky_hash` in traffic algorithms module and update references

--- snakeway-core/src/traffic/algorithms/mod.rs
+++ snakeway-core/src/traffic/algorithms/mod.rs
@@ -1,11 +1,11 @@
 mod failover;
-mod hash;
 mod least_connections;
 mod random;
 mod round_robin;
+mod sticky_hash;
 
 pub use failover::*;
-pub use hash::*;
 pub use least_connections::*;
 pub use random::*;
 pub use round_robin::*;
+pub use sticky_hash::*;
