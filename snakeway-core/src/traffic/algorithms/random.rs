1f01dc865db1704186cfdfda864edccb55fcd54a
Ethan Hann <ethanhann@gmail.com>
2025-12-28 16:31:15 -0500

    Implement random traffic decision logic

--- snakeway-core/src/traffic/algorithms/random.rs
+++ snakeway-core/src/traffic/algorithms/random.rs
@@ -1,11 +1,23 @@
 use crate::ctx::RequestCtx;
 use crate::traffic::{decision::*, snapshot::*, strategy::TrafficStrategy};
+use rand::{Rng, thread_rng};
 
 #[derive(Debug, Default)]
 pub struct Random {}
 
 impl TrafficStrategy for Random {
-    fn decide(&self, _req: &RequestCtx, _healthy: &[UpstreamSnapshot]) -> Option<TrafficDecision> {
-        todo!("implement random")
+    fn decide(&self, _req: &RequestCtx, healthy: &[UpstreamSnapshot]) -> Option<TrafficDecision> {
+        if healthy.is_empty() {
+            return None;
+        }
+
+        let idx = thread_rng().gen_range(0..healthy.len());
+        let upstream = &healthy[idx];
+
+        Some(TrafficDecision {
+            upstream_id: upstream.endpoint.id,
+            reason: DecisionReason::Random,
+            protocol: None,
+        })
     }
 }
