42e86e1b4e061a836dcdb2b70378bbebe9b44488
Ethan Hann <ethanhann@gmail.com>
2026-01-01 17:17:28 -0500

    Rename `validators` module to singular `validator` for consistency across the validation structure. Update imports and references accordingly.

--- snakeway-core/src/conf/validation/mod.rs
+++ snakeway-core/src/conf/validation/mod.rs
@@ -1,4 +1,4 @@
 pub mod error;
 pub mod validation;
 pub mod validation_ctx;
-mod validators;
+mod validator;
--- snakeway-core/src/conf/validation/validation.rs
+++ snakeway-core/src/conf/validation/validation.rs
@@ -1,6 +1,6 @@
 use crate::conf::types::{EntrypointConfig, RouteConfig, ServiceConfig};
 use crate::conf::validation::validation_ctx::{ValidationCtx, ValidationErrors};
-use crate::conf::validation::validators;
+use crate::conf::validation::validator;
 use std::collections::HashMap;
 
 /// Validate everything that exists in a fully parsed config.
@@ -11,13 +11,13 @@ pub fn validate_runtime_config(
 ) -> Result<(), ValidationErrors> {
     let mut ctx = ValidationCtx::default();
 
-    if let Err(e) = validators::validate_version(entry.server.version) {
+    if let Err(e) = validator::validate_version(entry.server.version) {
         ctx.push(e);
     } else {
-        validators::validate_server(&entry.server, &mut ctx);
-        validators::validate_listeners(&entry.listeners, &mut ctx);
-        validators::validate_routes(routes, services, &mut ctx);
-        validators::validate_services(services, &mut ctx);
+        validator::validate_server(&entry.server, &mut ctx);
+        validator::validate_listeners(&entry.listeners, &mut ctx);
+        validator::validate_routes(routes, services, &mut ctx);
+        validator::validate_services(services, &mut ctx);
     }
 
     ctx.into_result()
