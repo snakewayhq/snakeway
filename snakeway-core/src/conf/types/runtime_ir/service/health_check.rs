89a9fba57cfecd416198c5f520b08a38df9cc891
Ethan Hann <ethanhann@gmail.com>
2026-01-08 14:02:01 -0500

    Remove unused default function for `enable` in `HealthCheckConfig`

--- snakeway-core/src/conf/types/runtime_ir/service/health_check.rs
+++ snakeway-core/src/conf/types/runtime_ir/service/health_check.rs
@@ -2,16 +2,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize, Default)]
 pub struct HealthCheckConfig {
-    #[serde(default = "hc_default_enable")]
     pub enable: bool,
     #[serde(default = "hc_default_threshold")]
     pub failure_threshold: u32,
     #[serde(default = "hc_default_unhealthy_cooldown_seconds")]
     pub unhealthy_cooldown_seconds: u64,
-}
-
-fn hc_default_enable() -> bool {
-    true
 }
 
 fn hc_default_threshold() -> u32 {
