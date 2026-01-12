c7df07bf03f4592e84351fdcbe2e3e4e61ded0e4
Ethan Hann <ethanhann@gmail.com>
2026-01-12 07:03:05 -0500

    Refactor device configuration: rename `device.rs` to `device_spec.rs` for clarity and update module imports accordingly.

--- snakeway-core/src/conf/types/specification/device/mod.rs
+++ snakeway-core/src/conf/types/specification/device/mod.rs
@@ -1,9 +1,9 @@
-mod device;
+mod device_spec;
 mod identity;
 mod structured_logging;
 mod wasm;
 
-pub use device::*;
+pub use device_spec::*;
 pub use identity::*;
 pub use structured_logging::*;
 pub use wasm::*;
