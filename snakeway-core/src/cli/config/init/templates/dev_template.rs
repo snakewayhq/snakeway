c5842ddb5d6dea8fa5a7c2856967c7acd499b414
Ethan Hann <ethanhann@gmail.com>
2026-02-14 10:27:56 -0500

    Refactor `config init` templates: consolidate device file generation logic, add support for new device specs, and streamline file creation process.

--- snakeway-core/src/cli/config/init/templates/dev_template.rs
+++ snakeway-core/src/cli/config/init/templates/dev_template.rs
@@ -1,5 +1,7 @@
 use crate::conf::types::{
     BindInterfaceInput, BindSpec, DevicesFile, IdentityDeviceSpec, IngressSpec,
+    NetworkPolicyDeviceSpec, RequestFilterDeviceSpec, RequestRateLimitingDeviceSpec,
+    StructuredLoggingDeviceSpec,
 };
 use crate::serialization::to_hcl_string;
 use std::collections::HashMap;
@@ -10,15 +12,50 @@ pub(crate) fn generate(
     ingress_dir_path: PathBuf,
     files_to_create: &mut HashMap<PathBuf, String>,
 ) -> Result<(), anyhow::Error> {
-    let identity_device_file = DevicesFile {
-        identity_device: Some(IdentityDeviceSpec::default()),
-        ..Default::default()
-    };
+    let device_files = HashMap::from([
+        (
+            "request_filter.hcl",
+            DevicesFile {
+                request_filter_device: Some(RequestFilterDeviceSpec::default()),
+                ..Default::default()
+            },
+        ),
+        (
+            "identity.hcl",
+            DevicesFile {
+                identity_device: Some(IdentityDeviceSpec::default()),
+                ..Default::default()
+            },
+        ),
+        (
+            "network_policy.hcl",
+            DevicesFile {
+                network_policy_device: Some(NetworkPolicyDeviceSpec::default()),
+                ..Default::default()
+            },
+        ),
+        (
+            "request_rate_limiting.hcl",
+            DevicesFile {
+                request_rate_limiting_device: Some(RequestRateLimitingDeviceSpec::default()),
+                ..Default::default()
+            },
+        ),
+        (
+            "structured_logging.hcl",
+            DevicesFile {
+                structured_logging_device: Some(StructuredLoggingDeviceSpec::default()),
+                ..Default::default()
+            },
+        ),
+    ]);
 
-    files_to_create.insert(
-        device_dir_path.join("identity.hcl"),
-        to_hcl_string(&identity_device_file)?,
-    );
+    for (file_name, file_content) in device_files {
+        files_to_create.insert(
+            device_dir_path.join(file_name),
+            to_hcl_string(&file_content)?,
+        );
+    }
 
     let httpbin_ingress_spec = IngressSpec {
         bind: Some(BindSpec {
