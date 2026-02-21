18fe4d4a981425be2f97915d73deb3580244ec9b
Ethan Hann <ethanhann@gmail.com>
2026-02-21 09:10:26 -0500

    Add v0.9.0 release notes and update documentation navigation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -64,6 +64,7 @@ export default defineConfig({
             },
             {
                 label: 'Release Notes', items: [
+                    {label: 'v0.9.0 (dev)', link: '/releases/v0_9_0/'},
                     {label: 'v0.8.0 (latest)', link: '/releases/v0_8_0/'},
                     {label: 'v0.7.0', link: '/releases/v0_7_0/'},
                     {label: 'v0.6.0', link: '/releases/v0_6_0/'},
