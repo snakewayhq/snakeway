543219160a69b9dd95bb7042905358c9e509156e
Ethan Hann <ethanhann@gmail.com>
2026-03-07 20:33:03 -0500

    refactor: separate request execution framework into distinct submodule

---
+++ snakeway-core/src/execution/mod.rs
@@ -0,0 +1,5 @@
+pub mod ctx;
+pub mod device;
+pub mod enrichment;
+pub mod route;
+pub mod traffic;
--- snakeway-core/src/lib.rs
+++ snakeway-core/src/lib.rs
@@ -12,20 +12,17 @@ mod proxy;
 pub mod cert_manager;
 pub mod cli;
 pub mod conf;
-pub mod ctx;
-pub mod device;
-mod enrichment;
 pub mod http_event;
 pub mod observability;
-pub mod route;
 pub mod runtime;
 mod serialization;
 pub mod server;
-pub mod traffic_management;
 pub mod ws_connection_management;
 
 //-----------------------------------------------------------------------------
 // Public / Feature-gated
 //-----------------------------------------------------------------------------
+mod control_plane;
+pub mod execution;
 #[cfg(feature = "static_files")]
 pub mod static_files;
