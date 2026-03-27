5ce01a2cced0dda9f4b19a37760234d63ac04455
Ethan Hann <ethanhann@gmail.com>
2026-03-27 17:36:20 -0400

    Refactor ingress module: consolidate exports using wildcard re-exports
    Change ingress submodules from `pub mod` to private `mod` declarations and use wildcard `pub use` statements to re-export types. Remove redundant re-exports from parent `specification/mod.rs` that are now handled by ingress module. Reorder module declarations alphabetically.

--- crates/snakeway-conf/src/types/specification/ingress/mod.rs
+++ crates/snakeway-conf/src/types/specification/ingress/mod.rs
@@ -1,11 +1,15 @@
-pub mod bind;
-pub mod bind_admin_spec;
-pub mod bind_interface_spec;
-mod ingress_spec;
-
+mod bind;
+mod bind_admin_spec;
 mod bind_admin_validation;
-pub mod service;
-pub mod static_files_spec;
+mod bind_interface_spec;
+mod ingress_spec;
+mod service;
+mod static_files_spec;
 mod static_files_validation;
 
+pub use bind::*;
+pub use bind_admin_spec::*;
+pub use bind_interface_spec::*;
 pub use ingress_spec::*;
+pub use service::*;
+pub use static_files_spec::*;
--- crates/snakeway-conf/src/types/specification/mod.rs
+++ crates/snakeway-conf/src/types/specification/mod.rs
@@ -1,17 +1,11 @@
 mod device;
+mod entrypoint_spec;
 mod ingress;
 mod origin;
 mod server;
 
-mod entrypoint_spec;
-
 pub use device::*;
 pub use entrypoint_spec::*;
-pub use ingress::bind::*;
-pub use ingress::bind_admin_spec::*;
-pub use ingress::bind_interface_spec::*;
-pub use ingress::service::*;
-pub use ingress::static_files_spec::*;
 pub use ingress::*;
 pub use origin::*;
 pub use server::*;
