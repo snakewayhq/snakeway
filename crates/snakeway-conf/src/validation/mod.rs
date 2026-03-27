aaf416e35d46442d980581fde9b0136ab25cfcba
Ethan Hann <ethanhann@gmail.com>
2026-03-27 16:43:25 -0400

    Rename validation modules: `intra_file` → `single_file`, `cross_file` → `multi_file`
    This is a reversion essentially. The new naming convention didn't sit well.

--- crates/snakeway-conf/src/validation/mod.rs
+++ crates/snakeway-conf/src/validation/mod.rs
@@ -1,7 +1,7 @@
-mod cross_file;
 mod error;
-mod intra_file;
+mod multi_file;
 mod report;
+mod single_file;
 mod spec_impls;
 mod validate;
 mod validate_spec_trait;
@@ -9,7 +9,7 @@ mod validated_config;
 pub(crate) mod validator;
 
 #[cfg(test)]
-pub(crate) use intra_file::*;
+pub(crate) use single_file::*;
 pub(crate) use validate::validate_spec;
 pub(crate) use validated_config::ValidatedConfig;
 pub(crate) use validator::*;
