4e054d9bfb18b22db987834e52afd2ddfcfb8af1
Ethan Hann <ethanhann@gmail.com>
2026-03-31 10:21:58 -0400

    Refactor bind_admin module: split into separate files in subdirectory

--- crates/snakeway-conf/src/types/specification/ingress/mod.rs
+++ crates/snakeway-conf/src/types/specification/ingress/mod.rs
@@ -1,13 +1,12 @@
 mod bind;
-mod bind_admin_spec;
-mod bind_admin_validation;
+mod bind_admin;
 mod bind_interface_spec;
 mod ingress_spec;
 mod service;
 mod static_files;
 
 pub use bind::*;
-pub use bind_admin_spec::*;
+pub use bind_admin::bind_admin_spec::*;
 pub use bind_interface_spec::*;
 pub use ingress_spec::*;
 pub use service::*;
