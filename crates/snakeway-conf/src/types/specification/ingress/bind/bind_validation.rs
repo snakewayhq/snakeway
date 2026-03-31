e6dce48ec0e2c484e0eb750c2cee620283e96fc2
Ethan Hann <ethanhann@gmail.com>
2026-03-31 10:40:04 -0400

    Add comments to BindSpec validation implementation

--- crates/snakeway-conf/src/types/specification/ingress/bind/bind_validation.rs
+++ crates/snakeway-conf/src/types/specification/ingress/bind/bind_validation.rs
@@ -4,10 +4,12 @@ use crate::validation::{ValidateSpec, ValidationReport};
 
 impl ValidateSpec for BindSpec {
     fn validate(&self, origin: &Origin, report: &mut ValidationReport) {
+        // Port validation.
         if !is_valid_port(self.port) {
             report.invalid_port(self.port, origin);
         }
 
+        // Connection filters.
         if let Some(connection_filter) = &self.connection_filter {
             connection_filter.validate(origin, report);
         }
@@ -16,10 +18,12 @@ impl ValidateSpec for BindSpec {
             connection_rate_limiting_filter.validate(origin, report);
         }
 
+        // TLS cert/key/acme validation.
         if let Some(tls) = &self.tls {
             tls.validate(origin, report);
         }
 
+        // Redirect HTTP to HTTPS validation.
         if let Some(redirect) = &self.redirect_http_to_https {
             redirect.validate(origin, report);
         }
